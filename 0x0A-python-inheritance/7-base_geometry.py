6bd1ecaa2be440b80989c5ee799bf2b3e5b65079
Achiengotieno6 <audrey.achieng@gmail.com>
2023-07-10 19:39:36 -0700

    correction of codestyle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,13 +5,13 @@
 class BaseGeometry:
     """defines the class BaseGeometry"""
 
-def area(self):
+    def area(self):
         """Raises an Exception"""
         raise Exception("area() is not implemented")
 
-def integer_validator(self, name, value):
-    """validates the value"""
-    if (not type(value) is int):
-        raise TypeError("{} must be an integer".format(name))
-    if value <= 0:
-        raise ValueError("{} must b greater than 0".format(name))
+    def integer_validator(self, name, value):
+        """validates the value"""
+        if type(value) is not int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
