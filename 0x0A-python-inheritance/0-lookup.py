4e393fafd27851bde3d8eac361b76bca0c8795fc
Achiengotieno6 <audrey.achieng@gmail.com>
2023-07-10 18:37:49 -0700

    ptyhon inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Returns a list of available attributes and methods"""
+
+
+def lookup(obj):
+    """Returns a list of attributes and methods of object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""creates an empty class"""
+
+
+class BaseGeometry:
+    """defines the class BaseGeometry"""
+    pass
