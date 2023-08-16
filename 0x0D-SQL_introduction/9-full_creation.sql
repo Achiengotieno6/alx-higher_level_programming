3cba068fe8fe56d69abae3e552a9175862dc9b17
Achiengotieno6 <audrey.achieng@gmail.com>
2023-08-16 00:14:38 -0700

    correction by use of backticks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,17 @@
 -- creates a table second_table in the database
+-- create four records
 
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
 
--- Creates four records
-
-INSERT INTO `second_table` (`id`, `name` `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name` `score`) VALUES (2, "Alexa", 3);
-INSERT INTO `second_table` (`id`, `name` `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name` `score`) VALUES (4, "George", 8);
+INSERT INTO `second_table` (`id`, `name` `score`)
+VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name` `score`)
+VALUES (2, "Alexa", 3);
+INSERT INTO `second_table` (`id`, `name` `score`)
+VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name` `score`)
+VALUES (4, "George", 8);
