740fb844f7a813406102a893f45a62cf1c420c27
Achiengotieno6 <audrey.achieng@gmail.com>
2023-08-15 19:25:13 -0700

    added comments

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---creates a database
+-- creates a database
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,1 +1,3 @@
+-- deletes the database in MySQL server.
+
 DROP DATABASE IF EXISTS hbtn_0c_0;
