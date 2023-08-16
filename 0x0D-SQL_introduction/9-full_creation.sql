b399b17c23666bad4a0791416954475366994550
Achiengotieno6 <audrey.achieng@gmail.com>
2023-08-15 19:39:22 -0700

    fixing of code lines

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
--- Create four records
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
-	(1, 'John', 10),
-	(2, 'Alex', 3),
-	(3, 'Bob', 14),
-	(4, 'George', 8);
+-- Creates four records
+
+INSERT INTO 'second_table' ('id', 'name' 'score') VALUES (1, "John", 10);
+INSERT INTO 'second_table' ('id', 'name' 'score') VALUES (2, "Alexa", 3);
+INSERT INTO 'second_table' ('id', 'name' 'score') VALUES (3, "Bob", 14);
+INSERT INTO 'second_table' ('id', 'name' 'score') VALUES (4, "George", 8);
